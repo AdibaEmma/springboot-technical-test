29a9bc003f4e474c2fd5dcd19b16eec677f87bdb
Emmanuel Adiba <eabaagah@gmail.com>
2022-06-14 14:13:46 +0000

    add country model

---
+++ src/main/java/com/aweperi/agpaytechtechnicaltest/model/Country.java
@@ -0,0 +1,10 @@
+package com.aweperi.agpaytechtechnicaltest.model;
+
+import lombok.Data;
+
+@Data
+public class Country {
+    private final Long id;
+    private final String name;
+    private final Integer population;
+}
