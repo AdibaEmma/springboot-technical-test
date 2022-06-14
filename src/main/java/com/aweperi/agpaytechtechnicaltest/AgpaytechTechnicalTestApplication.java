0b4fe40a82c99b495c6c1a33cace28d68b92caf8
Emmanuel Adiba <eabaagah@gmail.com>
2022-06-14 14:08:00 +0000

    first commit

---
+++ src/main/java/com/aweperi/agpaytechtechnicaltest/AgpaytechTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.aweperi.agpaytechtechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgpaytechTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgpaytechTechnicalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aweperi/agpaytechtechnicaltest/AgpaytechTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aweperi.agpaytechtechnicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgpaytechTechnicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
